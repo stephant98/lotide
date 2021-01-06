3abe1891e863bbfbb349bb61b4d80e96fbde3617
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-05 21:02:46 -0500

    linting eqArrays.js

--- eqArrays.js
+++ eqArrays.js
@@ -6,16 +6,16 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const eqArrays = function(array1 , array2){
-  if (array1.length !== array2.length){
+const eqArrays = function(array1 , array2) {
+  if (array1.length !== array2.length) {
     return false;
   } else {
-    for (let i = 0; i < array1.length; i++){
-      if (array1[i] !== array2[i]){
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
         return false;
-      } 
+      }
     } return true;
-  } 
+  }
 };
 
 
