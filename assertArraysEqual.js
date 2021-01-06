1eab1b4403da5823cc207a4032b5a7f8ce7951bf
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-05 21:09:10 -0500

    linted assertArraysEqual.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,22 +1,22 @@
-const eqArrays = function(array1 , array2){
-  if (array1.length !== array2.length){
+const eqArrays = function(array1 , array2) {
+  if (array1.length !== array2.length) {
     return false;
   } else {
-    for (let i = 0; i < array1.length; i++){
-      if (array1[i] !== array2[i]){
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
         return false;
-      } 
+      }
     } return true;
-  } 
-};
-
-const assertArraysEqual = function(array1 , array2){
-  if(eqArrays(array1 , array2)){
-    console.log(`✅✅✅ Assertion Passed: The arrays are equal!`)
-  } else {
-    console.log(`❌❌❌ Assertion Failed: The arrays are not equal!`)
   }
 };
 
-assertArraysEqual([1, 2, 3], [1, 2, 3])
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4])
+const assertArraysEqual = function(array1 , array2) {
+  if (eqArrays(array1 , array2)) {
+    console.log(`✅✅✅ Assertion Passed: The arrays are equal!`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: The arrays are not equal!`);
+  }
+};
+
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
