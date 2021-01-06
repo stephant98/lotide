fac68e59c109c4580d98bc430ff34810a1dc7f12
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-06 13:05:38 -0500

    added middle function to retrieve middle elements

--- middle.js
+++ middle.js
@@ -17,3 +17,26 @@ const assertArraysEqual = function(array1 , array2) {
     console.log(`❌❌❌ Assertion Failed: The arrays are not equal!`);
   }
 };
+
+const middle = function(arr) {
+  let middleVal;
+  if (arr.length === 1 || arr.length === 2) {
+    return [];
+  } else if (arr.length % 2 !== 0) {
+    middleVal = [arr[Math.floor(arr.length / 2)]];
+  } else if (arr) {
+    middleVal = [arr[Math.floor((arr.length - 1) / 2)] , arr[Math.floor(arr.length / 2)]];
+  }
+  return middleVal;
+};
+
+// first else if for even length arrays
+// second else ir  for odd length arrays
+
+
+console.log(middle([1]));
+console.log(middle([1, 2, 3, 4]));
+console.log(middle([1, 2, 3]));
+
+assertArraysEqual(middle(["hello", "hola", "bien"]), ["hola"]);
+assertArraysEqual(middle(["middle"]), ["middle"]);
