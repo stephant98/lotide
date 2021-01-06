de940570bda3e8b0c6c9219daf0e5819ec2ceccd
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-05 20:53:35 -0500

    linted head.js

--- head.js
+++ head.js
@@ -6,9 +6,9 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-const head = function(actual){
+const head = function(actual) {
   return actual[0];
-}
+};
 
 
 assertEqual(head([5, 6, 7]), 5);
