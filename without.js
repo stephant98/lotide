675ae6743fcf6e36dc87d9690d6da7f6e646b0a5
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-05 20:51:36 -0500

    created without function for new array

--- without.js
+++ without.js
@@ -17,3 +17,22 @@ const assertArraysEqual = function(array1 , array2){
     console.log(`❌❌❌ Assertion Failed: The arrays are not equal!`)
   }
 };
+
+const without = function (arr, notWanted){
+  let output = [];
+  for (let i = 0; i < arr.length; i++){
+    if(arr[i] !== notWanted[i]){
+      output.push(arr[i]);
+    }
+  } return output
+}
+
+console.log(without([1, 2, 3], [1])) // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
+console.log(without(["hola", "senor"], ["hola", "senora", "buenos dias"]))
+
+const words = ["hello", "world", "lighthouse"];
+without(words, ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
+
