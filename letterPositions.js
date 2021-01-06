e7adce26566466c530957339f4d9ad8df5af7a71
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-06 18:08:27 -0500

    for loop to create object inside letterpositions function

--- letterPositions.js
+++ letterPositions.js
@@ -1,19 +1,16 @@
-const eqArrays = function(array1 , array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
+const letterPositions = function(sentence) {
+  const results = {};
+  // iterate over sentence
+  for (let i = 0; i < sentence.length; i++) {
+    if (results[sentence[i]]) {
+      results[sentence[i]].push(i);
+    } else {
+      if (sentence[i] !== " ") {
+        results[sentence[i]] = [i];
       }
-    } return true;
-  }
+    }
+  } return results;
 };
 
-const assertArraysEqual = function(array1 , array2) {
-  if (eqArrays(array1 , array2)) {
-    console.log(`✅✅✅ Assertion Passed: The arrays are equal!`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: The arrays are not equal!`);
-  }
-};
+console.log(letterPositions("lighthouse in the house"));
+
