149f2d1a85619b4740fe9a0b9806f089bf7d169e
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-05 21:11:57 -0500

    linted without.js

--- without.js
+++ without.js
@@ -1,35 +1,35 @@
-const eqArrays = function(array1 , array2){
-  if (array1.length !== array2.length){
+const eqArrays = function(array1 , array2) {
+  if (array1.length !== array2.length) {
     return false;
   } else {
-    for (let i = 0; i < array1.length; i++){
-      if (array1[i] !== array2[i]){
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
         return false;
-      } 
+      }
     } return true;
-  } 
-};
-
-const assertArraysEqual = function(array1 , array2){
-  if(eqArrays(array1 , array2)){
-    console.log(`✅✅✅ Assertion Passed: The arrays are equal!`)
-  } else {
-    console.log(`❌❌❌ Assertion Failed: The arrays are not equal!`)
   }
 };
 
-const without = function (arr, notWanted){
+const assertArraysEqual = function(array1 , array2) {
+  if (eqArrays(array1 , array2)) {
+    console.log(`✅✅✅ Assertion Passed: The arrays are equal!`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: The arrays are not equal!`);
+  }
+};
+
+const without = function(arr, notWanted) {
   let output = [];
-  for (let i = 0; i < arr.length; i++){
-    if(arr[i] !== notWanted[i]){
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] !== notWanted[i]) {
       output.push(arr[i]);
     }
-  } return output
-}
+  } return output;
+};
 
-console.log(without([1, 2, 3], [1])) // => [2, 3]
-console.log(without(["1", "2", "3"], [1, 2, "3"])) // => ["1", "2"]
-console.log(without(["hola", "senor"], ["hola", "senora", "buenos dias"]))
+console.log(without([1, 2, 3], [1])); // => [2, 3]
+console.log(without(["1", "2", "3"], [1, 2, "3"])); // => ["1", "2"]
+console.log(without(["hola", "senor"], ["hola", "senora", "buenos dias"]));
 
 const words = ["hello", "world", "lighthouse"];
 without(words, ["lighthouse"]); // no need to capture return value for this test case
