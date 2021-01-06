8773e745bcdbd7774d30f73bc99f49c4728cf091
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-06 15:54:42 -0500

    countLetters function that returns object with count of each letter within parameter

--- countLetters.js
+++ countLetters.js
@@ -5,3 +5,22 @@ const assertEqual = function(actual, expected) {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+const countLetters = function(sentence) {
+  const letterCount = {};
+
+  for (const letter of sentence) {
+    if (letterCount[letter]) {
+      letterCount[letter] += 1;
+    } else {
+      if (letter !== " ") {
+        letterCount[letter] = 1;
+      }
+    }
+  } return letterCount;
+};
+
+
+assertEqual(countLetters("hola me llamo stephan").h, 2);
+assertEqual(countLetters("hola me llamo stephan").o, 2);
+assertEqual(countLetters("hola me llamo stephan").l, 2);
