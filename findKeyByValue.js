8f8f77293bd5e0877050d9703fda3132211a076e
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-06 19:26:05 -0500

    findKeyByValue function and test cases

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -5,3 +5,28 @@ const assertEqual = function(actual, expected) {
     console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
   }
 };
+
+
+const findKeyByValue = function(obj, value) {
+  let keys = Object.keys(obj);
+  for (const key of keys) {
+    if (obj[key] == value) {
+      return key;
+    }
+  } return;
+};
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sci_fi");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "naruto"), "comedy");
