c2f6559e9572241f058a84639ae516b326c6f587
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-07 18:32:17 -0500

    added assertArraysEqual and eqArrays functions and test cases

--- map.js
+++ map.js
@@ -1,3 +1,24 @@
+const eqArrays = function(array1 , array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    } return true;
+  }
+};
+
+const assertArraysEqual = function(array1 , array2) {
+  if (eqArrays(array1 , array2)) {
+    console.log(`✅✅✅ Assertion Passed: The arrays are equal!`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: The arrays are not equal!`);
+  }
+};
+
+
 
 const words = ["familia", "christian", "lia", "erich", "stephan"];
 const venezuelaCities = ["caracas", "valencia", "maracaibo"];
@@ -18,3 +39,5 @@ const results2 = map(venezuelaCities, venezuelaCity => venezuelaCity[1])
 console.log(results2)
 
 
+assertArraysEqual(results1, ["f", "c", "l", "e", "s"])
+assertArraysEqual(results2, ["a", "a", "a"])
