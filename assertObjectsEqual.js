a380b6af564076b6bf6bc667eec9766944d03f90
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-08 18:41:04 -0500

    added test cases and llinted file

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -32,11 +32,30 @@ const eqObjects = function(object1, object2) {
 
 
 const assertObjectsEqual = function(actual, expected) {
-  const inspect = require('util').inspect; // <= add this line
-  if(eqObjects(actual, expected)){
+  const inspect = require('util').inspect;
+  if (eqObjects(actual, expected)) {
     console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
   } else {
-    console.log(`🛑🛑🛑 Assertion Failed: Assertion Failed: ${inspect(actual)} === ${inspect(expected)}`)
+    console.log(`🛑🛑🛑 Assertion Failed: Assertion Failed: ${inspect(actual)} === ${inspect(expected)}`);
   }
 };
 
+const hola = { a: [1, 2, 3], b: [3, 3, 4]};
+const hello = { a: [1, 2, 3], b:[3, 2, 4]};
+assertObjectsEqual(hola, hello);
+
+
+const ab = { a: "1", b: "2" };
+const ba = { b: "2", a: "1" };
+assertObjectsEqual(ab, ba);
+
+const abc = { a: "1", b: "2", c: "3" };
+assertObjectsEqual(ab, abc);
+
+
+const cd = { c: "1", d: ["2", 3] };
+const dc = { d: ["2", 3], c: "1" };
+assertObjectsEqual(cd, dc);
+
+const cd2 = { c: "1", d: ["2", 3, 4] };
+assertObjectsEqual(cd, cd2);
