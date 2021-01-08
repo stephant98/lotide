a94607ef9410367cf5aca4dd02d5e1a859a5ebb8
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-08 18:33:20 -0500

    added assertObjectsEqual function to file

--- assertObjectsEqual.js
+++ assertObjectsEqual.js
@@ -16,3 +16,14 @@ const eqObjects = function(object1, object2) {
     }
   } return true;
 };
+
+
+const assertObjectsEqual = function(actual, expected) {
+  const inspect = require('util').inspect; // <= add this line
+  if(eqObjects(actual, expected)){
+    console.log(`✅✅✅ Assertion Passed: ${inspect(actual)} === ${inspect(expected)}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: Assertion Failed: ${inspect(actual)} === ${inspect(expected)}`)
+  }
+};
+
