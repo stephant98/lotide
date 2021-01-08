881949e02229ed6347f73cd7856bb032372aa19e
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-07 19:42:12 -0500

    added assertArraysEqual to test case takeUntil function

--- takeUntil.js
+++ takeUntil.js
@@ -1,3 +1,25 @@
+const eqArrays = function(array1 , array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  } else {
+    for (let i = 0; i < array1.length; i++) {
+      if (array1[i] !== array2[i]) {
+        return false;
+      }
+    } return true;
+  }
+};
+
+const assertArraysEqual = function(array1 , array2) {
+  if (eqArrays(array1 , array2)) {
+    console.log(`✅✅✅ Assertion Passed: The arrays are equal!`);
+  } else {
+    console.log(`❌❌❌ Assertion Failed: The arrays are not equal!`);
+  }
+};
+
+
+
 const takeUntil = function(array, callback) {
   let accumulator = [];
 
@@ -19,3 +41,5 @@ const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Re
 const results2 = takeUntil(data2, x => x === ',');
 console.log(results2);
 
+assertArraysEqual(results, [1, 2, 5, 7, 2]);
+assertArraysEqual(results2, ["I've", "been", "to", "Hollywood"]);
