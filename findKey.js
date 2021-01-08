f5eb68c25882bfe9c82e209fb89d52705c904f32
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-07 20:47:18 -0500

    added findkey function and test cases

--- findKey.js
+++ findKey.js
@@ -6,3 +6,33 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const findKey = function(obj, callback) {
+  for (const property in obj) {
+    if (callback(obj[property])) {
+      return property;
+    }
+  } return;
+};
+
+const result = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2); // => "noma"
+
+
+
+assertEqual(result, "noma");
+
+
+
+const result2 = findKey({
+  "maracaibo": { hotness : "very"},
+  "valencia": { hotness: "soso"},
+  "caracas": { hotness: "nothing"}
+}, item => item.hotness === "very");
+
+assertEqual(result2, "maracaibo");
