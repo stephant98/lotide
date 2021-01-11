ec59f9cf373f21eb81273a7289f04db517f810f5
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-11 14:39:37 -0500

    added mocha chai test cases

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,6 +1,18 @@
+const assert = require('chai').assert;
 const assertEqual = require('../assertEqual');
 const tail = require('../tail');
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+
+describe('#tail', () => {
+  it("should return ['Lighthouse', 'Labs'] for ['Yo Yo', 'Lighthouse', 'Labs']", () => {
+    assert.deepEqual(tail(["Yo Yo", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("should return an empty array for ['hola']", () => {
+    assert.deepEqual(tail(['hola']), []);
+  });
+
+  it('should return an empty array for an empty array', () => {
+    assert.deepEqual(tail([]), []);
+  });
+});
