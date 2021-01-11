1c56f046842d5f18ffd039f9abb676849ac10cda
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-11 12:55:07 -0500

    adding module exports and removing test cases and assertEqual function

--- tail.js
+++ tail.js
@@ -1,16 +1,8 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-
 const tail = function(actual) {
   let newTail = actual.slice(1);
   return newTail;
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+
+
+module.exports = tail;
