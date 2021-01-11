41b41234b82b3cad9588525c78ea094535ba90db
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-11 14:54:33 -0500

    deleted old commented test cases

--- test/middleTest.js
+++ test/middleTest.js
@@ -21,9 +21,3 @@ describe('#middle', () => {
 
 });
 
-/*console.log(middle([1]));
-console.log(middle([1, 2, 3, 4]));
-console.log(middle([1, 2, 3]));
-
-assertArraysEqual(middle(["hello", "hola", "bien"]), ["hola"]);
-assertArraysEqual(middle(["middle"]), ["middle"]);*/
