d5d6f2acbc02847dca119255db7fce3bac21d41a
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-11 13:26:32 -0500

    removed eqArrays and assertArraysEqual functions

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-const eqArrays = function(array1 , array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    } return true;
-  }
-};
-
-const assertArraysEqual = function(array1 , array2) {
-  if (eqArrays(array1 , array2)) {
-    console.log(`✅✅✅ Assertion Passed: The arrays are equal!`);
-  } else {
-    console.log(`❌❌❌ Assertion Failed: The arrays are not equal!`);
-  }
-};
-
 const middle = function(arr) {
   let middleVal;
   if (arr.length === 1 || arr.length === 2) {
