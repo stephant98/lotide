4f40207ae5967dd899bcb3690b417c7118d1818c
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-11 13:15:47 -0500

    added module.exports and required eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -10,5 +10,3 @@ const assertArraysEqual = function(array1 , array2) {
 
 module.exports = assertArraysEqual;
 
-assertArraysEqual([1, 2, 3], [1, 2, 3]);
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
