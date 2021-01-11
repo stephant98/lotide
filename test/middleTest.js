b81086f3ea78bea19e67c4fd7d7ae8a2d0ba9a46
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-11 14:53:45 -0500

    added all mocha chai test cases

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,9 +1,29 @@
+const assert = require('chai').assert;
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-console.log(middle([1]));
+
+describe('#middle', () => {
+  it('should return ["hola"] for ["hello", "hola", "bien"]', () => {
+    assert.deepEqual(middle(["hello", "hola", "bien"]), ["hola"]);
+  });
+
+  it('should return an empty array for one element array ["middle"]', () => {
+    assert.deepEqual(middle(["middle"]), []);
+  });
+
+  it('should return two elements [2, 3] for an even length array [1, 2, 3, 4]', () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+
+  it('should return one elements [2] for an odd length array [1, 2, 3]', () => {
+    assert.deepEqual(middle([1, 2, 3]), [2]);
+  });
+
+});
+
+/*console.log(middle([1]));
 console.log(middle([1, 2, 3, 4]));
 console.log(middle([1, 2, 3]));
 
 assertArraysEqual(middle(["hello", "hola", "bien"]), ["hola"]);
-assertArraysEqual(middle(["middle"]), ["middle"]);
+assertArraysEqual(middle(["middle"]), ["middle"]);*/
