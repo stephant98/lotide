3d48175f85684797411f7c365fd770239f58e248
Stephan Truchsess <stephantruchsess@gmail.com>
2021-01-11 13:11:36 -0500

    added module exports and required eqArrays function

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,4 @@
-const eqArrays = function(array1 , array2) {
-  if (array1.length !== array2.length) {
-    return false;
-  } else {
-    for (let i = 0; i < array1.length; i++) {
-      if (array1[i] !== array2[i]) {
-        return false;
-      }
-    } return true;
-  }
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = function(array1 , array2) {
   if (eqArrays(array1 , array2)) {
@@ -17,6 +7,8 @@ const assertArraysEqual = function(array1 , array2) {
     console.log(`❌❌❌ Assertion Failed: The arrays are not equal!`);
   }
 };
+
+module.exports = assertArraysEqual;
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]);
 assertArraysEqual([1, 2, 3], [1, 2, 3, 4]);
